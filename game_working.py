adc334047aae644b992ea0e0f61c3beeba831a55
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-02 15:36:05 -0700

    Check for Faulty User Input using isdigit

---
+++ game_working.py
@@ -0,0 +1,30 @@
+"""A number-guessing game."""
+
+# Put your code here
+import random
+first_name = input("Howdy, what's your name?")
+print("{}, I'm thinking of a number between 1 and 100\nTry to guess my number".format(first_name))
+
+random_number = random.randint(1,100)
+print(random_number)
+guess_count = 0
+
+while True:
+	guess_count += 1
+	guess = input("What is your guess?")
+	if guess.isdigit() is False: 
+		print("this is not a number! Try again ")
+	else: 
+		guess = int(guess)
+		if  guess < 1 or guess >100 : 
+			print("wow! you don't know your numbers! enter a VALID number!")
+		else: 
+			if guess > random_number:
+				print("Your guess is too high, try again")
+			elif guess < random_number:
+				print("Your guess is too low, try again")
+			else:
+				print("Well done, {}! You found my number in {} tries!".format(first_name, guess_count))
+				break
+	
+
