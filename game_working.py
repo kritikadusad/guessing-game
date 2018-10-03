2f2cd38dea4f6d189012c25add5566c34b680468
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-03 12:26:58 -0700

    Guessing game working but it does not exit if the user does not want to play again. Need to fix

--- game_working.py
+++ game_working.py
@@ -3,29 +3,49 @@
 # Put your code here
 import random
 first_name = input("Howdy, what's your name?")
-print("{}, I'm thinking of a number between 1 and 100\nTry to guess my number".format(first_name))
 
-random_number = random.randint(1,100)
-print(random_number)
-guess_count = 0
+guess_list = []
 
-while True:
-	guess_count += 1
-	try:
-		guess = int(input("What is your guess?"))
-	except ValueError: 
-		print("this is not a number! Try again ")
-		continue
+def guessing_number(): 
+	global guess_list
+	print("{}, I'm thinking of a number between 1 and 100\nTry to guess my number".format(first_name))
+	random_number = random.randint(1,100)
+	print(random_number)
+	guess_count = 0
+	
+	while True:
+		guess_count += 1
+		try:
+			guess = int(input("What is your guess?"))
+		except ValueError: 
+			print("this is not a number! Try again ")
+			continue
 
-	if  guess < 1 or guess >100 : 
-		print("wow! you don't know your numbers! enter a VALID number!")
-	else: 
-		if guess > random_number:
-			print("Your guess is too high, try again")
-		elif guess < random_number:
-			print("Your guess is too low, try again")
-		else:
-			print("Well done, {}! You found my number in {} tries!".format(first_name, guess_count))
-			break
+		if  guess < 1 or guess >100 : 
+			print("wow! you don't know your numbers! enter a VALID number!")
+		else: 
+			if guess > random_number:
+				print("Your guess is too high, try again")
+			elif guess < random_number:
+				print("Your guess is too low, try again")
+			else:
+				print("Well done, {}! You found my number in {} tries! ".format(first_name, guess_count))
+				restart = input("Would you like to play again? ")
+				guess_list.append(guess_count)
+				break
+	best_score = min(guess_list)
+	print("Your lowest number of guesses has been {}!".format(best_score))
+	return restart
+	
+
+
+while True: 
+	game = guessing_number()
+	if game == 'yes':
+		guessing_number()
+		break
+	
+		
+		
 
 
